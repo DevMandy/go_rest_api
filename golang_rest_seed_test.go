233ab18eb98001ebd2cd0cc2488a85296b525095
Mandy.Hubbard <mhubbard@myhomepay.com>
2016-11-29 15:59:01 -0600

    Adding Ginkgo test

---
+++ golang_rest_seed_test.go
@@ -0,0 +1,15 @@
+package main_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("GolangRestSeed", func() {
+
+	It("should be true", func() {
+		Expect(true).Should(BeTrue())
+
+	})
+
+})
